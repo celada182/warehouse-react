5292a3cd315bfda52a2aad467aa1d5dd48e2a9e5
Javier Celada <jcelada@theworkshop.com>
2023-06-16 16:01:57 +0200

    Stock calculated

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -1,22 +1,27 @@
 const Product = require("../model/product.model");
+const Article = require("../model/article.model");
 const importProducts = (req, res) => {
   readRequest(req.body, res);
 };
 
-const getProducts = (req, res) => {
-  const pageSize = req.query.pageSize || process.env.DEAFULT_PAGE_SIZE;
+const getProducts = async (req, res) => {
+  const pageSize = req.query.page_size || process.env.DEAFULT_PAGE_SIZE;
   const offset = req.query.offset || 0;
-  Product.find({}, null, {skip: offset, limit: pageSize})
-  .then(products => {
-    const response = products.map(product => {
-      const json = product.toJSON();
-      return {
-        name: json.name,
-        contain_articles: json.contain_articles
-      }
-    });
-    res.json(response);
-  });
+  let response = [];
+  const products = await Product.find({}, null,
+      {skip: offset, limit: pageSize});
+  for (const product of products) {
+    const processedProduct = await processProduct(product, response);
+    response.push(processedProduct);
+  }
+  res.json(response);
+}
+
+async function processProduct(product, response) {
+  const json = product.toJSON();
+  const articles = await findArticles(json.contain_articles);
+  const stock = calculateStock(json.contain_articles, articles);
+  return {name: product.name, stock: stock};
 }
 
 function readRequest(body, res) {
@@ -41,6 +46,26 @@ function insertProduct(product) {
     .catch(
         error => console.error("Error updating product:", product.name, error));
   }
+}
+
+function calculateStock(contain_articles, articles) {
+  let stock = 0;
+  for (const contain_article of contain_articles) {
+    const article = articles.find(a => a._id === contain_article.art_id);
+    const availableProducts = Math.floor(article.stock / contain_article.amount_of);
+    if (stock === 0) {
+      stock = availableProducts;
+    }
+    if (availableProducts < stock) {
+      stock = availableProducts;
+    }
+  }
+  return stock;
+}
+
+async function findArticles(contain_articles) {
+  const ids = contain_articles.map(article => article.art_id);
+  return Article.find({'_id': {$in: ids}});
 }
 
 module.exports = {importProducts: importProducts, getProducts: getProducts};
