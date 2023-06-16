5512423cd336643622f4731b353dcacb3ddb4790
Javier Celada <jcelada@theworkshop.com>
2023-06-16 08:18:57 +0200

    Set up environment

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const inventory = require('./routes/inventory');
 const product = require('./routes/product');
 
 const app = express();
+const DEFAULT_PORT = 3000;
 
 app.use(express.json());
 
 app.use('/inventory', inventory);
 app.use('/product', product);
 
-app.listen(3000, () => {
-  console.log(`Server Started at ${3000}`)
+dotenv.config();
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`)
 })
